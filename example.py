7513f51ac5beaf463fae5baed868449c4cc34d20
vthglyk <vasgl@intracom-telecom.com>
2018-12-11 11:39:36 +0200

    Added Comparison plots

--- example.py
+++ example.py
@@ -2,64 +2,69 @@ import os
 
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
 
 def main():
-	matplotlib.rcParams['text.usetex'] = True
-	sns.set(font_scale=5)
-	sns.set(style="whitegrid")
-	base_dir = "results/testRAP_29_2_18/exp_rap_false_AllData/exp_rap_false"
-	num_req_field = "Number of Requests"
-	times_field = "Response Times (ms)"
-	show_outliers = False
-	reqs = []
-	times = []
+    matplotlib.rcParams['text.usetex'] = True
+    sns.set(font_scale=1.5, style="whitegrid")
+    base_dir = "results/results_SearchTest 10_12_18_factory_registered10000_searchByName_false/search_add5_repeat10_byclient_byname_registered_10000_false"
+    num_req_field = "Number of Concurrent Requests"
+    times_field = "Response Times (s)"
+    title = "Search with 10000 registered resources without 2-way authentication"
+    base_filename = "search_stress_test_10_12_10000_false"
+    show_outliers = False
+    ymax = 40.0000000000001
+    ytick = 5
+    reqs = []
+    times = []
 
-	files = os.listdir(base_dir)
+    files = os.listdir(base_dir)
 
-	for f_name in files:
-		try:
-			num_req = int(f_name.split('_')[1])
-		except IndexError:
-			num_req = -1
+    for f_name in files:
+        try:
+            num_req = int(f_name.split('_')[1])
+        except IndexError:
+            num_req = -1
 
-		if 0 <= num_req < 100:
-			if num_req % 5 != 0:
-				continue
-			with open(base_dir + "/" + f_name, 'rb') as f:
-				for line in f.readlines()[:num_req]:
-					reqs.append(num_req)
-					times.append(int(line.split(' ')[2]))
+        if 0 <= num_req <= 100:
+            if num_req % 5 != 0:
+                continue
+            with open(base_dir + "/" + f_name, 'rb') as f:
+                for line in f.readlines()[:num_req]:
+                    reqs.append(num_req)
+                    times.append(int(line.split(' ')[2]) / 1000.0)
 
-	data_frame = pd.DataFrame({num_req_field: reqs, times_field: times})
-	response_times_boxplot = pd.melt(data_frame, id_vars=num_req_field, value_name=times_field)
+    data_frame = pd.DataFrame({num_req_field: reqs, times_field: times})
+    response_times_boxplot = pd.melt(data_frame, id_vars=num_req_field, value_name=times_field)
 
-	font = {
-		'family': 'Liberation Sans',
-		'weight': 'normal',
-		'size': 22
-	}
+    font = {
+        'family': 'Liberation Sans',
+        'weight': 'normal'
+    }
 
-	plt.rc('font', **font)
-	# plt.xlabel("x label")
-	# plt.ylabel("y label")
+    plt.rc('font', **font)
+    plt.yticks(np.arange(0, ymax, ytick))
+    # plt.xlabel("x label")
+    # plt.ylabel("y label")
 
-	plt.title("Stress Test")
-	# plt.ylim(ym)
-	# plt.legend(['True Positive Ratio'], loc='lower right')
-	# plt.legend(loc='upper right', prop={'size': 40})
-	sns.boxplot(x=num_req_field, y=times_field, data=response_times_boxplot, showfliers=show_outliers)
-	# plt.grid(axis='y')
-	# plt.grid(axis='x')
-	fig = plt.gcf()
-	fig.tight_layout()
-	fig.set_size_inches(10, 7)
-	# plt.savefig(base_filename + ".eps")
-	#
-	plt.show()
+    plt.title(title)
+    plt.ylim(ymax=ymax)
+    # plt.legend(['True Positive Ratio'], loc='lower right')
+    # plt.legend(loc='upper right', prop={'size': 40})
+    sns.boxplot(x=num_req_field, y=times_field, data=response_times_boxplot, showfliers=show_outliers, notch=True)
+    # plt.grid(axis='y')
+    # plt.grid(axis='x')
+    fig = plt.gcf()
+    # fig.tight_layout(pad=0.7 * 22 / font_size)
+    fig.tight_layout()
+    fig.set_size_inches(10, 7)
+    # plt.show()
+    plt.savefig("pdf/" + base_filename + ".pdf")
+    #
 
 
 if __name__ == "__main__":
-	main()
+    main()
---
+++ rap_comparison.py
@@ -0,0 +1,93 @@
+import os
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+def main():
+    matplotlib.rcParams['text.usetex'] = True
+    sns.set(font_scale=1.7, style="whitegrid")
+    base_dir = "results/"
+    x_label = "Number of Concurrent Requests"
+    y_label = "Median Response Times (s)"
+    title = "RAP Comparison"
+    base_filename = "rap_comparison"
+    y_max = 7.0000000000001
+    y_tick = 1
+    x_min = 5
+    x_max = 50
+    x_tick = 5
+    linewidth = 3
+    reqs = np.arange(x_min, x_max + 1, x_tick)
+    times = []
+
+    rap_files = [("Without server authentication", "results4_12_18_b/false/rap_add5_repeat10false"),
+                 ("With server authentication", "results4_12_18_b/true/rap_add5_repeat10true")]
+
+    for rap_file in rap_files:
+        folder = base_dir + rap_file[1]
+        files = os.listdir(folder)
+        times_experiment = {}
+
+        for f_name in files:
+
+            try:
+                num_req = int(f_name.split('_')[1])
+            except IndexError:
+                num_req = -1
+
+            if 0 <= num_req <= 100:
+                if num_req % 5 != 0:
+                    continue
+
+                if num_req not in times_experiment:
+                    times_experiment[num_req] = []
+
+                with open(folder + "/" + f_name, 'rb') as f:
+                    for line in f.readlines()[:num_req]:
+                        times_experiment[num_req].append(int(line.split(' ')[2]) / 1000.0)
+
+        medians = []
+        for key in reqs:
+            medians.append(np.median(times_experiment[key]))
+
+        times.append(medians)
+
+    print times
+
+    font = {
+        'family': 'Liberation Sans',
+        'weight': 'normal'
+    }
+
+    plt.rc('font', **font)
+    plt.yticks(np.arange(0, y_max, y_tick))
+    plt.xticks(np.arange(0, x_max, x_tick))
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    plt.title(title)
+    plt.ylim(ymax=y_max)
+    plt.xlim(xmax=x_max)
+
+    for i in np.arange(0, len(times), 1):
+        plt.plot(reqs, times[i], "-", lw=linewidth, label=rap_files[i][0])
+
+    plt.legend(['True Positive Ratio'], loc='lower right')
+    plt.legend(loc='upper left', prop={'size': 22})
+
+    # plt.grid(axis='y')
+    # plt.grid(axis='x')
+    fig = plt.gcf()
+    # fig.tight_layout(pad=0.7 * 22 / font_size)
+    fig.tight_layout()
+    fig.set_size_inches(10, 7)
+    # plt.show()
+    plt.savefig("pdf/" + base_filename + ".pdf")
+    #
+
+
+if __name__ == "__main__":
+    main()
